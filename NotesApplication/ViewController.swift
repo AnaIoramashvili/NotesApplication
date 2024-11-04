af3f139ee3b2762d58ce79f55e74ce5cced9c2f2
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2024-11-04 17:36:41 +0400

    Initial Commit

---
+++ NotesApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApplication
+//
+//  Created by Ana on 11/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
